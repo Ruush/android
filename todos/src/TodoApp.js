31822ea2dea1bbb3f80e3cce9b6bce62bfc79261
Ruush <gabriel.alvesferreira@gmail.com>
2019-06-27 13:37:01 -0300

    criacao do todoapp

---
+++ todos/src/TodoApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class TodoApp extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Alo Corno!</Text>
+            </View>
+        )
+    }
+}
